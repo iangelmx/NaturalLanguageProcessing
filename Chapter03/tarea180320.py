2198ac5cb83a26c99fdca3d07a354f279338b85e
Angel N Ramírez <iangelmx@hotmail.com>
2018-03-20 15:29:17 -0600

    Se actualiza con el ejercicio que dejó la maestra en clase de collocations del e960401 y separar el texto por oraciones.

--- Chapter03/tarea180320.py
+++ Chapter03/tarea180320.py
@@ -4,9 +4,33 @@ import nltk, re, pprint
 from urllib.request import *
 from bs4 import BeautifulSoup
 import feedparser
+#import functionsNLP
 url="http://www.gutenberg.org/files/2554/2554-0.txt"
 #url="http://www.gutenberg.org/files/2554/2554.txt"
 
+def separaPorOraciones(cadena):
+	sent_tokenizer = nltk.data.load("nltk:tokenizers/punkt/english.pickle")
+	listaOraciones=sent_tokenizer.tokenize(cadena)
+	return listaOraciones
+
+def getTextTokens(rutaArchivo, keepNumbers=False, backTextString=False):	#Una manera que se me ocurrió de tokenizar. Es precisa en un 96.35%
+	ruta=rutaArchivo
+	#print("ruta->"+str(ruta))
+	entrada= open(rutaArchivo, mode="r", encoding="utf-8")
+	abc=entrada.read()
+	abc=abc.lower()
+	tokens = nltk.tokenize.word_tokenize(str(abc))
+	if keepNumbers == False:
+		alphatokens = [word for word in tokens if word.isalpha()]
+		tokens=alphatokens				#Libera los tokens de números y símbolos raros
+		if backTextString == True:
+			return [tokens, abc]
+	return tokens
+
+texto="Hola a todos. ¿Cómo se encuentran el día de hoy?. Espero que muy bien, ya que yo estoy muy contento de verlos."
+print(separaPorOraciones(texto))
+
+"""
 raw = urlopen(url).read().decode('utf8')
 type(raw)
 print(len(raw))
@@ -16,9 +40,23 @@ print(raw[:75])
 tokens = nltk.word_tokenize(raw)
 print(len(tokens))
 print(tokens[:10])
+"""
+
+#text=nltk.Text(tokens)
+[tokens, cadena] = getTextTokens("e960401_txt.txt", backTextString=True)
 
 text=nltk.Text(tokens)
+raw = cadena.encode('utf8')
 text.collocations()
+print("")
+lista = separaPorOraciones(raw)
+
+for elemento in lista:
+	print(elemento)
+
+input("Fin")
+
+#text.collocations()
 
 print(raw.find("PART I"))
 
