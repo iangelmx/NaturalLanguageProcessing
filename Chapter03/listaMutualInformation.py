9c1f92980c69aa0d76ce64124b2eb205ab46c91b
Angel N Ramírez <iangelmx@hotmail.com>
2018-03-20 20:03:42 -0600

    Se ponen avances de la tarea de MutualInfo

--- Chapter03/listaMutualInformation.py
+++ Chapter03/listaMutualInformation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import functionsNLP
+from functionsNLP import *
+import math
 
 def getJointProbability(palabra1, palabra2, listaOraciones):
 	conteoW1=0
@@ -17,11 +18,58 @@ def getJointProbability(palabra1, palabra2, listaOraciones):
 			conteoW1W2+=1
 	pXw1 = (conteoW1) / (N)
 	pXw2 = (conteoW2) / (N)
-	
+	pXw1w2 = (conteoW1W2) / (N)
+	return [pXw1, pXw2,pXw1w2]
 
+
+#def calculateProbabilityWordsAbscents():
+	
+rutaArchivo="e960401_txt.txt"
 [tokens, textoCompleto] = getTextTokens(rutaArchivo, backTextString=True)
 
 listaOraciones = separaPorOraciones(textoCompleto)
+
+#Palabra base= "gobierno"
+
+MI={}
+
+for palabra in tokens:
+	input(palabra)
+	#Probabilidad de que W1 esté en oracion: p_w1_1
+	[p_w11, p_w21,p_w11_w21] = getJointProbability('gobierno', palabra, listaOraciones)
+	p_w10 = 1- p_w11
+	p_w20 = 1- p_w21
+	
+	p_w10_w21 = p_w21 - p_w11_w21
+	p_w10_w20 = p_w20 - p_w10_w21
+	p_w11_w20 = p_w20 - p_w10_w20
+
+	p = {}
+	p['w10w20']= p_w10_w20
+	p['w10w21']= p_w10_w21
+	p['w11w20']= p_w11_w20
+	p['w11w21']= p_w11_w21
+	p['w10']= p_w10
+	p['w11']= p_w11
+	p['w20']= p_w20
+	p['w21']= p_w21
+
+	try:
+		for u in range (0, 2):
+			for v in range (0, 2):
+				#print('w1'+str(u)+'w2'+str(v)+" * log2("+'w1'+str(u)+'w2'+str(v)+" / "+'w1'+str(u)+" * "+'w2'+str(v)+" )")
+				MI[palabra]= p['w1'+str(u)+'w2'+str(v)] * ( math.log2( (p['w1'+str(u)+'w2'+str(v)])/( (p['w1'+str(u)])*(p['w2'+str(v)]) ) ) )
+				print("MI['"+palabra+"'] = "+str(MI[palabra]))
+	except Exception as ex:
+		print(ex)
+		pass
+
+archivo = open("SALIDA.txt", "w")
+for elemento in MI:
+	archivo.write(elemento)
+
+
+
 
 
 
