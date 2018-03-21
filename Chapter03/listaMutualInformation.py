cddfb7dcfb341a2aac03eae0353120c962dbc057
iangelmx <iangelmx@hotmail.com>
2018-03-20 23:02:01 -0600

    Se respalda tarea y resultado de tarea MutualInformationOfGobierno

--- Chapter03/listaMutualInformation.py
+++ Chapter03/listaMutualInformation.py
@@ -21,6 +21,46 @@ def getJointProbability(palabra1, palabra2, listaOraciones):
 	pXw1w2 = (conteoW1W2) / (N)
 	return [pXw1, pXw2,pXw1w2]
 
+def getMutualInformationOfGobierno():
+	MI={}
+	a=0
+
+	for palabra in tokens:
+		#input(palabra)
+		#Probabilidad de que W1 esté en oracion: p_w1_1
+		[p_w11, p_w21,p_w11_w21] = getJointProbability('gobierno', palabra, listaOraciones)
+		p_w10 = 1- p_w11
+		p_w20 = 1- p_w21
+		
+		p_w10_w21 = p_w21 - p_w11_w21
+		p_w10_w20 = p_w20 - p_w10_w21
+		p_w11_w20 = p_w20 - p_w10_w20
+
+		p = {}
+		p['w10w20']= p_w10_w20
+		p['w10w21']= p_w10_w21
+		p['w11w20']= p_w11_w20
+		p['w11w21']= p_w11_w21
+		p['w10']= p_w10
+		p['w11']= p_w11
+		p['w20']= p_w20
+		p['w21']= p_w21
+
+		try:
+			for u in range (0, 2):
+				for v in range (0, 2):
+					#print('w1'+str(u)+'w2'+str(v)+" * log2("+'w1'+str(u)+'w2'+str(v)+" / "+'w1'+str(u)+" * "+'w2'+str(v)+" )")
+					MI[palabra]= p['w1'+str(u)+'w2'+str(v)] * ( math.log2( (p['w1'+str(u)+'w2'+str(v)])/( (p['w1'+str(u)])*(p['w2'+str(v)]) ) ) )
+					#print("MI['"+palabra+"'] = "+str(MI[palabra]))
+		except Exception as ex:
+			#print(ex)
+			pass
+		a+=1
+		if a%5000 == 0:
+			print("Working Hard. A-> "+str(a))
+			print("MI['"+palabra+"'] = "+str(MI[palabra]))
+
+	return MI
 
 #def calculateProbabilityWordsAbscents():
 	
@@ -31,43 +71,20 @@ listaOraciones = separaPorOraciones(textoCompleto)
 
 #Palabra base= "gobierno"
 
-MI={}
+#diccionarioMI=getMutualInformationOfGobierno()
 
-for palabra in tokens:
-	input(palabra)
-	#Probabilidad de que W1 esté en oracion: p_w1_1
-	[p_w11, p_w21,p_w11_w21] = getJointProbability('gobierno', palabra, listaOraciones)
-	p_w10 = 1- p_w11
-	p_w20 = 1- p_w21
-	
-	p_w10_w21 = p_w21 - p_w11_w21
-	p_w10_w20 = p_w20 - p_w10_w21
-	p_w11_w20 = p_w20 - p_w10_w20
 
-	p = {}
-	p['w10w20']= p_w10_w20
-	p['w10w21']= p_w10_w21
-	p['w11w20']= p_w11_w20
-	p['w11w21']= p_w11_w21
-	p['w10']= p_w10
-	p['w11']= p_w11
-	p['w20']= p_w20
-	p['w21']= p_w21
 
-	try:
-		for u in range (0, 2):
-			for v in range (0, 2):
-				#print('w1'+str(u)+'w2'+str(v)+" * log2("+'w1'+str(u)+'w2'+str(v)+" / "+'w1'+str(u)+" * "+'w2'+str(v)+" )")
-				MI[palabra]= p['w1'+str(u)+'w2'+str(v)] * ( math.log2( (p['w1'+str(u)+'w2'+str(v)])/( (p['w1'+str(u)])*(p['w2'+str(v)]) ) ) )
-				print("MI['"+palabra+"'] = "+str(MI[palabra]))
-	except Exception as ex:
-		print(ex)
-		pass
-
+"""
 archivo = open("SALIDA.txt", "w")
-for elemento in MI:
-	archivo.write(elemento)
+archivo.write("Gobierno...\n")
 
+for elemento in MI:
+	archivo.write(elemento+" : "+str(MI[elemento])+"\n")
+archivo.close()
+
+print("Proceso finalizado")
+"""
 
 
 
