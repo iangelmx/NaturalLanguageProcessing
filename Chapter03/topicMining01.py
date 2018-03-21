fa1251aa34a98ac0dc497380613c36445375a088
Angel N Ramírez <iangelmx@hotmail.com>
2018-03-21 16:23:12 -0600

    Se añade el archivo para la tarea

---
+++ Chapter03/topicMining01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#Segmentar en N artículos el archivo e960401.html
+
+#Tarea: Segmentar nuestro archivo html en artículos, como archivos o como elementos de arreglos.
+#		Calcular la frecuencia(conteo) de cada palabra del vocabulario
+#OPC:	Si se puede, quitar stopwords
+#OBLIG:	Chp 3 NLP with Python pp 93-106
