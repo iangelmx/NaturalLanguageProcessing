6af6dc46b68590bdaead774e155d4ece56c0e94f
Angel N Ramírez <iangelmx@hotmail.com>
2018-04-02 16:22:07 -0500

    Se complemeta con smoothing

--- Chapter03/tfIdfWords.py
+++ Chapter03/tfIdfWords.py
@@ -10,7 +10,7 @@ def calculaTF(palabra, vocabTokens):	#Se utiliza conteoPuro de tokens en texto
 	tf = math.log2(1+ocurrencias[palabra])
 	return tf
 
-def calculaIDF(palabra, vocabTokens, textoCompletoStr): #Se usan oraciones como documentos
+def calculaIDF(palabra, vocabTokens, textoCompletoStr, smooth=False): #Se usan oraciones como documentos
 	listaOraciones = separaPorOraciones(textoCompletoStr)
 	M = len(listaOraciones)
 	k = 0
@@ -18,7 +18,10 @@ def calculaIDF(palabra, vocabTokens, textoCompletoStr): #Se usan oraciones como 
 		for oracion in listaOraciones:
 			if palabra in oracion:
 				k+=1
-		idf = math.log2((M+1)/(k))
+		if smooth == False:
+			idf = math.log2((M+1)/(k))
+		elif smooth == True:
+			idf = math.log2((M+1+0.5)/(k+1))
 		return idf
 	except Exception as ex:
 		print("Error con: "+palabra+"\n"+str(ex))
@@ -41,18 +44,20 @@ idfs = {}
 
 transaccion = []
 transaccion.append("START TRANSACTION;")
+transaccion.append("TRUNCATE tfs_idfs;")
 
 for palabra in listaPalabras:
-	idf = calculaIDF(palabra, tokens,textoStr)
+	idf = calculaIDF(palabra, tokens,textoStr, smooth=True)
 	tf = calculaTF(palabra, tokens)
 	try:
 		tfIdf=0.0
 		tfIdf = (float(tf)*float(idf)) # <----------------- ???
-		transaccion.append("UPDATE tfs_idfs set tf_idf= '"+str(tfIdf)+"' WHERE token = '"+palabra+"';")
+		#transaccion.append("UPDATE tfs_idfs set tf_idf= '"+str(tfIdf)+"' WHERE token = '"+palabra+"';")
+		transaccion.append("INSERT INTO tfs_idfs (token, tf, idf, tfIdf) VALUES('"+palabra+"', '"+str(tf)+"', '"+str(idf)+"', '"+str(tfIdf)+"');")
 	except Exception as ex:
+		transaccion.append("INSERT INTO tfs_idfs (token, tf, idf, tfIdf) VALUES('"+palabra+"', '"+str(tf)+"', '"+str(idf)+"', '"+str(tfIdf)+"');")
 		print(ex)
 
-	#transaccion.append("INSERT INTO tfs_idfs (token, tf, idf) VALUES('"+palabra+"', '"+str(tf)+"', '"+str(idf)+"');")
 transaccion.append("COMMIT;")
 
 print(doTransaction(transaccion))
