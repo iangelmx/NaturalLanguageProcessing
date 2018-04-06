e402fbaea5c43c2bc7c803e87bc79832948c3995
iangelmx <iangelmx@hotmail.com>
2018-04-06 01:40:37 -0500

    Se termina tarea para el 180406

--- Chapter03/tareaFor180406.py
+++ Chapter03/tareaFor180406.py
@@ -6,6 +6,28 @@ from mariamysqlib import *
 from collections import defaultdict
 from nltk.corpus import brown
 
+"""
+text = word_tokenize("And now for something completely different")
+nltk.pos_tag(text)
+
+tagged_token = nltk.tag.str2tuple('fly/NN')
+print(tagged_token)
+print(tagged_token[0])
+print(tagged_token[1])
+
+sent = '''
+The/AT grand/JJ jury/NN commented/VBD on/IN a/AT number/NN of/IN
+other/AP topics/NNS ,/, AMONG/IN them/PPO the/AT Atlanta/NP and/CC
+Fulton/NP-tl County/NN-tl purchasing/VBG departments/NNS which/WDT it/PPS
+said/VBD ``/`` ARE/BER well/QL operated/VBN and/CC follow/VB generally/RB
+accepted/VBN practices/NNS which/WDT inure/VB to/IN the/AT best/JJT
+interest/NN of/IN both/ABX governments/NNS ''/'' ./.
+'''
+tuplas = [nltk.tag.str2tuple(t) for t in sent.split()]
+
+
+"""
+
 pos = defaultdict(list)
 
 pos['a']=['1', '2', '3']
