2ccaf17d9e8322737ed1bad61046cd12a0b838dc
iangelmx <iangelmx@hotmail.com>
2018-05-30 00:23:25 -0500

    Se añade script de BD para pos neg taggin

---
+++ deteccionDeSentimientos/BD/polaridadPosNegReviews.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-05-2018 a las 07:21:16
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nlp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `polaridadPosNegReviews`
+--
+
+CREATE TABLE `polaridadposnegreviews` (
+  `polaridad` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `rank` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `archivoPos` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `polaridadPosNegReviews`
+--
+ALTER TABLE `polaridadPosNegReviews`
+  ADD PRIMARY KEY (`archivoPos`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
